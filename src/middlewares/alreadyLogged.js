b3378e247e6df7c9b187f448c27d7968a7016562
Salavdor <salvador.stereo@gmail.com>
2023-12-14 02:37:23 -0600

    Delete src directory

--- src/middlewares/alreadyLogged.js
+++
@@ -1,8 +0,0 @@
-export const alreadyLogged = (req, res, next) => {
-  const estado = req.session.info || false;
-  if (estado.loggedIn == true) {
-    res.redirect("/products");
-  } else {
-    next();
-  }
-};
