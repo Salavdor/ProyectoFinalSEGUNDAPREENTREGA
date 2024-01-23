534303493a01ac4f56ff570f784191c670ca97c1
Salavdor <salvador.stereo@gmail.com>
2024-01-23 00:26:30 -0600

    Delete src directory

--- src/daos/filesystem/user.dao.js
+++
@@ -1,8 +0,0 @@
-import FSDao from "./fs.dao.js";
-const path = "./src/daos/filesystem/users.json";
-
-export default class UserDaoFS extends FSDao {
-    constructor() {
-        super(path);
-    }
-};
