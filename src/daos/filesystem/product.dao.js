c1d80abc9c066b168495c0cfe9dcfda56431f7ce
Salavdor <salvador.stereo@gmail.com>
2024-02-07 13:47:01 -0600

    Delete src directory

--- src/daos/filesystem/product.dao.js
+++
@@ -1,9 +0,0 @@
-import FSDao from "./fs.dao.js";
-const path = "./src/daos/filesystem/products.json";
-
-export default class ProductDaoFS extends FSDao {
-    constructor() {
-        super(path);
-    }
-};
-
