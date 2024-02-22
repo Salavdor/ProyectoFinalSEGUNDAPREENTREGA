ab997dcaa558f4033bc160cdc5426142cfcbdc30
Salavdor <salvador.stereo@gmail.com>
2024-02-22 17:46:51 -0600

    Delete src directory

--- src/dtos/ticket.dto.js
+++
@@ -1,9 +0,0 @@
-export default class TicketDTO {
-    constructor(ticket) {
-        this.code = ticket.code
-        this.purchase_datetime = ticket.purchase_datetime
-        this.amount = ticket.amount
-        this.purchaser = ticket.purchaser
-        this.products = ticket.products
-    }
-}
